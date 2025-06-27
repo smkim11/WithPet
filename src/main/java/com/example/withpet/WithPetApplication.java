2747113a68274040c05896439c3b87eea524047c
kim <smkim000721@naver.com>
2025-06-27 17:25:12 +0900

    withpet

---
+++ src/main/java/com/example/withpet/WithPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.withpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/withpet/WithPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
