755cc880ebb3741b644f5e3d51d56f3b791f362f
kim <smkim000721@naver.com>
2025-07-18 17:32:29 +0900

    파일업로드 설정 추가

--- src/main/java/com/example/withpet/WithPetApplication.java
+++ src/main/java/com/example/withpet/WithPetApplication.java
@@ -2,12 +2,18 @@ package com.example.withpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class WithPetApplication {
+public class WithPetApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WithPetApplication.class, args);
 	}
-
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/file/**").addResourceLocations("file:///C:/project/file/");
+	}
 }
