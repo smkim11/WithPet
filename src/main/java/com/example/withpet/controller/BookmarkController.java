e537241ca04ddb435aefe236c82799875f8187c8
kim <smkim000721@naver.com>
2025-07-21 17:41:16 +0900

    즐겨찾기 등록 추가

--- src/main/java/com/example/withpet/controller/BookmarkController.java
+++ src/main/java/com/example/withpet/controller/BookmarkController.java
@@ -1,8 +1,24 @@
 package com.example.withpet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.withpet.dto.Bookmark;
+import com.example.withpet.service.BookmarkService;
+import com.example.withpet.service.StoreService;
 
 @RestController
 public class BookmarkController {
-
+	@Autowired StoreService storeService;
+	@Autowired BookmarkService bookmarkService;
+	
+	// 즐겨찾기
+	@PostMapping("/insertBookmark")
+	public void insertBookmark(@RequestBody Bookmark bookmark) {
+		bookmarkService.insertBookmark(bookmark);
+	}
+	
+	
 }
--- src/main/java/com/example/withpet/service/BookmarkService.java
+++ src/main/java/com/example/withpet/service/BookmarkService.java
@@ -4,14 +4,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.withpet.dto.Bookmark;
+import com.example.withpet.entity.BookmarkEntity;
+import com.example.withpet.entity.StoreEntity;
+import com.example.withpet.entity.UserEntity;
 import com.example.withpet.repository.BookmarkRepository;
+import com.example.withpet.repository.StoreRepository;
+import com.example.withpet.repository.UserRepository;
 
 @Service
 public class BookmarkService {
 	@Autowired BookmarkRepository bookmarkRepository;
+	@Autowired UserRepository userRepository;
+	@Autowired StoreRepository storeRepository;
+	
+	// 이미등록 되어있는지 확인
+	public boolean findSameStore(int storeId) {
+		if(storeRepository.findById(storeId)==null) {
+			return true;
+		}
+		return false;
+	}
 	
 	// 즐겨찾기 등록
 	public void insertBookmark(Bookmark bookmark) {
+		BookmarkEntity bookmarkEntity = new BookmarkEntity();
+		UserEntity userEntity = userRepository.findById(bookmark.getUserId()).orElse(null);
+		StoreEntity storeEntity = storeRepository.findById(bookmark.getStoreId()).orElse(null);
 		
+		bookmarkEntity.setUserEntity(userEntity);
+		bookmarkEntity.setStoreEntity(storeEntity);
+		
+		bookmarkRepository.save(bookmarkEntity);
 	}
 }
