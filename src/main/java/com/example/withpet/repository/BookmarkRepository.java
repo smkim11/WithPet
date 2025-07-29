0269d63176a1f067616a3944699b2e61fa812df9
kim <smkim000721@naver.com>
2025-07-29 17:46:57 +0900

    유저ID와 가게ID로 즐겨찾기 번호 조회

--- src/main/java/com/example/withpet/repository/BookmarkRepository.java
+++ src/main/java/com/example/withpet/repository/BookmarkRepository.java
@@ -6,9 +6,18 @@ import java.util.Map;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.example.withpet.dto.Bookmark;
 import com.example.withpet.entity.BookmarkEntity;
 
 public interface BookmarkRepository extends JpaRepository<BookmarkEntity, Integer>{
+	
+	// 즐겨찾기 번호조회
+	@Query(nativeQuery = true,
+			value= "SELECT * "
+			+"FROM bookmark where user_id=:userId and store_id=:storeId")
+	BookmarkEntity findByUserIdAndStoreId(int userId, int storeId);
+	
+	
 	// 사용자별 즐겨찾기 목록
 	@Query(nativeQuery = true,
 			value= "SELECT s.title, s.category, s.address "
